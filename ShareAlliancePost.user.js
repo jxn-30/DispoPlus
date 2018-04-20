62fac37bd2075d1ca783fc33eabb4d923bf3b43e
Jan <Jan.litzenburger@gmail.com>
2018-04-20 18:31:29 +0200

    Update ShareAlliancePost.user.js

--- ShareAlliancePost.user.js
+++ ShareAlliancePost.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ShareAlliancePost
 // @namespace    Leitstellenspiel
-// @version      2.2
+// @version      2.3.1
 // @author       jalibu, JuMaHo
 // @include      https://www.leitstellenspiel.de/missions/*
 // ==/UserScript==
@@ -9,27 +9,24 @@
 (() => {
     'use strict';
 
-    let initButtons = () => {
-        let btnMarkup = '<a href="#" class="btn btn-success btn-sm alert_notify_alliance hidden-xs" title="Alarmieren, im Verband freigeben und Nachricht in Verbands-Chat"><img class="icon icons8-Phone-Filled" src="/images/icons8-phone_filled.svg" width="18" height="18"> <img class="icon icons8-Share" src="/images/icons8-share.svg" width="20" height="20"> <span class="glyphicon glyphicon-bullhorn" style="font-size: 13px;"></span></a>';
+    // Create Button and add event listener
+    const initButtons = () => {
+        const btnMarkup = '<a href="#" class="btn btn-success btn-sm alert_notify_alliance hidden-xs" title="Alarmieren, im Verband freigeben und Nachricht in Verbands-Chat"><img class="icon icons8-Phone-Filled" src="/images/icons8-phone_filled.svg" width="18" height="18"> <img class="icon icons8-Share" src="/images/icons8-share.svg" width="20" height="20"> <span class="glyphicon glyphicon-bullhorn" style="font-size: 13px;"></span></a>';
         $('.alert_next_alliance').parent().append(btnMarkup);
-        $('.alert_notify_alliance').click(() => {
-            processAllianceShare();
-        });
+        $('.alert_notify_alliance').click(processAllianceShare);
     };
 
-    let processAllianceShare = () => {
-        let message = 'Frei zum Sammeln!';
-        let sendToAlliance = 1;
-        let missionShareLink = $('#mission_alliance_share_btn').attr('href');
-        let missionId = missionShareLink.replace('/missions/','').replace('/alliance', '');
-        let csrfToken = $('meta[name="csrf-token"]').attr('content');
-        console.log('Share mission #' + missionId);
+    const processAllianceShare = () => {
+        const message = 'Frei zum Mitverdienen!';
+        const sendToAlliance = 1;
+        const missionShareLink = $('#mission_alliance_share_btn').attr('href');
+        const missionId = missionShareLink.replace('/missions/','').replace('/alliance', '');
+        const csrfToken = $('meta[name="csrf-token"]').attr('content');
+
         $('.alert_notify_alliance').html('Teilen..');
         $.get('/missions/' + missionId + '/alliance' , () => {
             $('.alert_notify_alliance').html('Chatten..');
-            console.log('Mission is shared... Now post message to chat...');
             $.post( "/mission_replies", {authenticity_token: csrfToken, mission_reply: {alliance_chat: sendToAlliance, content: message, mission_id: missionId}}, (data, status, xhr) => {
-                console.log('Message posted...');
                 $('.alert_notify_alliance').html('Alarmieren..');
                 $('#mission_alarm_btn').click();
             } );
